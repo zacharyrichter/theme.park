30c528ab6789a2b48a856fc5d9d4d5217495db43
Zach Richter <92696882+zacharyrichter@users.noreply.github.com>
2026-05-31 21:32:24 -0500

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,17 +1,13 @@
-
  //Custom Text Header //
  // ACSII slant font
-
 // Needs to be inside <section id="login" class="shadow">
-// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
-
 let custom_text_header = `
-<div class="custom-text-header"> <pre>
-    ____                   ______      __       
-   / __ )____  _________ _/ ____/_  __/ /_  ___ 
-  / __  / __ \/ ___/ __ `/ /   / / / / __ \/ _ \
- / /_/ / /_/ / /  / /_/ / /___/ /_/ / /_/ /  __/
-/_____/\____/_/   \__, /\____/\__,_/_.___/\___/ 
-                 /____/                         </pre> </div>
+<div class="custom-text-header"> <pre> 
+    ____  ____  ____  ______           ________  ______  ______
+   / __ )/ __ &#92;/ __ &#92;/ ____/          / ____/ / / / __ )/_____/
+  / __  / / / / /_/ / / _   ______   / /   / / / / __  / __/   
+ / /_/ / /_/ / _, _/ /_/ / /_____/  / /___/ /_/ / /_/ / /___   
+/_____/&#92;____/_/ |_|&#92;____/           &#92;____/&#92;____/_____/_____/   
+                                                        </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
